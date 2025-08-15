ec8ca40b308b819785cd210fa5c74984ab6b3c81
Abdullah Al Noman <111286942+abdlnoman@users.noreply.github.com>
2025-08-15 21:06:58 +0600

    solved problem01

---
+++ Problem-fiiinal.js
@@ -0,0 +1,9 @@
+function totalFine( fare ) {
+    if(typeof  fare !== "number" || fare<= 0) {
+        return "Invalid";
+    }
+    let surcharge = fare * 0.2;
+    let serviceCharge = 30;
+    let total = fare + surcharge +serviceCharge;
+    return total;
+}
